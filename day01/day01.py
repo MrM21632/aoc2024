49848b55ccbc03f670dd75f06f71fbbb0c1b1123
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-12-04 18:04:09 -0500

    Day 1 complete

---
+++ day01/day01.py
@@ -0,0 +1,48 @@
+import collections
+import re
+from typing import List
+
+
+def read_file(filename: str) -> List[str]:
+    with open(filename, "r") as file:
+        lines = file.readlines()
+    return lines
+
+def find_sum_of_distances(input_file: str) -> int:
+    first, second = [], []
+    lines = read_file(input_file)
+
+    for line in lines:
+        numbers = re.split(r'\s+', line)
+        first.append(int(numbers[0]))
+        second.append(int(numbers[1]))
+    
+    first.sort()
+    second.sort()
+    return sum(abs(first[i] - second[i]) for i in range(len(first)))
+
+def find_similarity_score(input_file: str) -> int:
+    first, second = [], []
+    lines = read_file(input_file)
+
+    for line in lines:
+        numbers = re.split(r'\s+', line)
+        first.append(int(numbers[0]))
+        second.append(int(numbers[1]))
+    
+    second_freq = collections.Counter(second)
+    total = 0
+    for f in first:
+        if f in second_freq:
+            total += (f * second_freq[f])
+    return total
+
+
+if __name__ == '__main__':
+    print('===== DAY 1, PUZZLE 1 =====')
+    print('The test input result is ', find_sum_of_distances('p1_test_input.txt'))
+    print('The main input result is ', find_sum_of_distances('p1_input.txt'))
+
+    print('\n\n===== DAY 1, PUZZLE 2 =====')
+    print('The test input result is ', find_similarity_score('p1_test_input.txt'))
+    print('The main input result is ', find_similarity_score('p1_input.txt'))
