1494d6d063cdc4bc21181a0508c283ec0f7270b1
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-12-05 09:39:23 -0500

    Part 1 done, boilerplating for Part 2

--- day05/day05.py
+++ day05/day05.py
@@ -7,18 +7,16 @@ def read_file(filename: str) -> List[str]:
         lines = file.readlines()
     return lines
 
-def construct_graph_and_indegree(rules_file: str) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
+def construct_rules_graph(rules_file: str) -> Dict[int, List[int]]:
     # Defaultdict allows us to correctly handle the deepest child nodes
     # (i.e., who have no children themselves) down the line.
     graph = collections.defaultdict(list)
-    indegree = collections.defaultdict(int)
 
     lines = read_file(rules_file)
     for line in lines:
         values = line.split('|')
         graph[int(values[0])].append(int(values[1]))
-        indegree[int(values[1])] += 1
-    return graph, indegree
+    return graph
 
 def construct_updates(updates_file: str) -> List[List[int]]:
     lines = read_file(updates_file)
@@ -27,8 +25,43 @@ def construct_updates(updates_file: str) -> List[List[int]]:
         result.append(list(map(int, line.split(','))))
     return result
 
+def group_updates(graph: Dict[int, List[int]], updates: List[List[int]]) -> Tuple[List[List[int]], List[List[int]]]:
+    correct, incorrect = [], []
+    for u in updates:
+        valid = True
+        for i in range(len(u) - 1):
+            if any(x not in graph[u[i]] for x in u[i + 1:]):
+                valid = False
+                break
+        if valid:
+            correct.append(u)
+        else:
+            incorrect.append(u)
+    return correct, incorrect
+
 def get_sum_of_middles(rules_file: str, updates_file: str) -> int:
-    pass
+    graph = construct_rules_graph(rules_file)
+    updates = construct_updates(updates_file)
+
+    correct, _ = group_updates(graph, updates)
+    total = 0
+    for c in correct:
+        if len(c) % 2 == 0:
+            total += c[len(c) // 2 - 1]
+        else:
+            total += c[len(c) // 2]
+    return total
+
+def get_sum_of_fixed_middles(rules_file: str, updates_file: str) -> int:
+    graph = construct_rules_graph(rules_file)
+    updates = construct_updates(updates_file)
+
+    _, incorrect = group_updates(graph, updates)
+    total = 0
+    for i in incorrect:
+        # TODO: Correctly order the list, find its middle
+        pass
+    return total
 
 
 if __name__ == '__main__':
@@ -37,5 +70,5 @@ if __name__ == '__main__':
     print('The main input result is ', get_sum_of_middles('input_rules.txt', 'input_updates.txt'))
 
     print('\n\n===== DAY 5, PUZZLE 2 =====')
-    # print('The test input result is ', find_all_x_mases('test_input.txt'))
-    # print('The main input result is ', find_all_x_mases('input.txt'))
+    print('The test input result is ', get_sum_of_fixed_middles('test_input_rules.txt', 'test_input_updates.txt'))
+    print('The main input result is ', get_sum_of_fixed_middles('input_rules.txt', 'input_updates.txt'))
