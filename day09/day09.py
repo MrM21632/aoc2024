4d531da46e0164ac3f362d0a279395e62af9fab0
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-12-10 16:48:23 -0500

    Day 9 complete

--- day09/day09.py
+++ day09/day09.py
@@ -1,4 +1,6 @@
-from typing import List
+import collections
+import heapq
+from typing import Dict, List, Tuple
 
 
 def read_file(filename: str) -> str:
@@ -13,14 +15,31 @@ def construct_disk(input_file: str) -> List[int]:
     on_empty_block = False
     curr_file_id = 0
     for c in content:
-        block_size = int(c)
+        length = int(c)
         if on_empty_block:
-            result.extend([-1] * block_size)
+            result.extend([-1] * length)
         else:
-            result.extend([curr_file_id] * block_size)
+            result.extend([curr_file_id] * length)
             curr_file_id += 1
         on_empty_block = not on_empty_block
     return result
+
+def get_groups_and_positions_on_disk(
+    input_file: str,
+) -> Tuple[Dict[int, Tuple[int, int]], Dict[int, List[int]]]:
+    lengths = [int(c) for c in read_file(input_file)]
+    files = dict()                         # File ID: (start position, length)
+    empty = collections.defaultdict(list)  # length: start positions
+
+    curr = 0
+    for i, l in enumerate(lengths):
+        if i & 1:
+            if l > 0:
+                heapq.heappush(empty[l], curr)
+        else:
+            files[i // 2] = (curr, l)
+        curr += l
+    return files, empty
 
 def compute_checksum(disk: List[int]) -> int:
     return sum([i * v for i, v in enumerate(disk) if v != -1])
@@ -42,12 +61,36 @@ def defrag_disk(input_file: str) -> int:
     
     return compute_checksum(disk)
 
+def defrag_without_refrag(input_file: str) -> int:
+    files, empty = get_groups_and_positions_on_disk(input_file)
+
+    # Defrag the disk by shifting whole files to empty blocks where they can fit
+    for id in sorted(files.keys(), reverse=True):
+        fs, fl = files[id]
+        candidates = sorted([(empty[l][0], l) for l in empty if l >= fl])
+        if candidates:
+            es, el = candidates[0]
+            if fs > es:
+                files[id] = (es, fl)
+                heapq.heappop(empty[el])
+                if not empty[el]:
+                    del empty[el]
+                
+                remaining = el - fl
+                if remaining:
+                    heapq.heappush(empty[remaining], es + fl)
+    
+    return sum(
+        id * (start * length + (length * (length - 1)) // 2)
+        for id, (start, length) in files.items()
+    )
+
 
 if __name__ == '__main__':
     print('===== DAY 9, PUZZLE 1 =====')
     print('The test input result is ', defrag_disk('test_input.txt'))
     print('The main input result is ', defrag_disk('input.txt'))
 
-    print('\n\n===== DAY 9, PUZZLE 2 =====')  # TODO
-    print('The test input result is ', defrag_disk('test_input.txt'))
-    print('The main input result is ', defrag_disk('input.txt'))
+    print('\n\n===== DAY 9, PUZZLE 2 =====')
+    print('The test input result is ', defrag_without_refrag('test_input.txt'))
+    print('The main input result is ', defrag_without_refrag('input.txt'))
