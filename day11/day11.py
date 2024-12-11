bb2ffb31b76be123007da8ca6108ec6583a17079
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-12-11 08:57:57 -0500

    Day 11 complete (memoization for the win)

--- day11/day11.py
+++ day11/day11.py
@@ -1,7 +1,5 @@
+import functools
 from typing import List
-
-
-TOTAL_BLINKS = 25
 
 
 def read_file(filename: str) -> str:
@@ -13,30 +11,35 @@ def construct_arrangement(input_file) -> List[int]:
     contents = read_file(input_file)
     return list(map(int, contents.strip().split()))
 
-def perform_blinks(input_file: str) -> int:
+def perform_blinks(input_file: str, blinks: int) -> int:
     stones = construct_arrangement(input_file)
 
-    for _ in range(25):
-        new_stones = []
-        for s in stones:
-            if s == 0:
-                new_stones.append(1)
-            elif len(str(s)) & 1:  # Odd length
-                new_stones.append(s * 2024)
-            else:  # Even length
-                s_str = str(s)
-                s_str_len = len(s_str) // 2
-                new_stones.extend([int(s_str[:s_str_len]), int(s_str[s_str_len:])])
-        stones = new_stones
-    return len(stones)
+    @functools.lru_cache(None)
+    def blink(stone: int, blinks_left: int) -> int:
+        if blinks_left == 0:
+            return 1
+        
+        result = 0
+        if stone == 0:
+            result += blink(1, blinks_left - 1)
+        elif len(str(stone)) & 1:  # Odd length
+            result += blink(stone * 2024, blinks_left - 1)
+        else:
+            stone_str = str(stone)
+            stone_str_len = len(stone_str) // 2
+            result += blink(int(stone_str[:stone_str_len]), blinks_left - 1)
+            result += blink(int(stone_str[stone_str_len:]), blinks_left - 1)
+        return result
+
+    return sum(blink(s, blinks) for s in stones)
 
 
 
 if __name__ == '__main__':
     print('===== DAY 9, PUZZLE 1 =====')
-    print('The test input result is ', perform_blinks('test_input.txt'))
-    print('The main input result is ', perform_blinks('input.txt'))
+    print('The test input result is ', perform_blinks('test_input.txt', 25))
+    print('The main input result is ', perform_blinks('input.txt', 25))
 
     print('\n\n===== DAY 9, PUZZLE 2 =====')
-    print('The test input result is ', perform_blinks('test_input.txt'))
-    print('The main input result is ', perform_blinks('input.txt'))
+    print('The test input result is ', perform_blinks('test_input.txt', 75))
+    print('The main input result is ', perform_blinks('input.txt', 75))
