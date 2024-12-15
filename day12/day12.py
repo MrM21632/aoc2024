0ce447bfb62f9f46328b3263923acc3a99627e77
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-12-15 00:38:45 -0500

    Day 12, Part 1

---
+++ day12/day12.py
@@ -0,0 +1,74 @@
+import collections
+from typing import List, Set, Tuple, TypeAlias
+
+
+Pair: TypeAlias = Tuple[int, int]
+
+
+DIRS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
+def read_file(filename: str) -> List[str]:
+    with open(filename, 'r') as file:
+        contents = file.readlines()
+    return contents
+
+def parse_grid(input_file: str) -> List[List[str]]:
+    lines = read_file(input_file)
+    return [list(line.strip()) for line in lines]
+
+def find_regions(grid: List[List[str]]) -> List[Set[Pair]]:
+    m, n = len(grid), len(grid[0])
+    is_valid = lambda r, c: 0 <= r < m and 0 <= c < n
+
+    def flood_fill(sr, sc):
+        region = set([(sr, sc)])
+        queue = collections.deque([(sr, sc)])
+
+        plant = grid[sr][sc]
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in DIRS:
+                nr, nc = r + dr, c + dc
+                if is_valid(nr, nc) and (nr, nc) not in region and grid[nr][nc] == plant:
+                    region.add((nr, nc))
+                    queue.append((nr, nc))
+        return region
+    
+    regions = []
+    filled = set()
+    for i in range(m):
+        for j in range(n):
+            if (i, j) in filled:
+                continue
+            region = flood_fill(i, j)
+            filled |= region
+            regions.append(region)
+    return regions
+
+def compute_perimeter(region: Set[Pair]) -> int:
+    result = 0
+    for r, c in region:
+        for dr, dc in DIRS:
+            if (r + dr, c + dc) not in region:
+                result += 1
+    return result
+
+def get_price_of_fencing(input_file: str) -> int:
+    grid = parse_grid(input_file)
+    regions = find_regions(grid)
+
+    total_price = 0
+    for region in regions:
+        total_price += len(region) * compute_perimeter(region)
+    return total_price
+
+
+if __name__ == '__main__':
+    print('===== DAY 12, PUZZLE 1 =====')
+    print('The test input result is ', get_price_of_fencing('test_input.txt'))
+    print('The main input result is ', get_price_of_fencing('input.txt'))
+
+    print('\n\n===== DAY 12, PUZZLE 2 =====')
+    print('The test input result is ', get_price_of_fencing('test_input.txt'))
+    print('The main input result is ', get_price_of_fencing('input.txt'))
